f371b079826985f3eb2d2ea6b4cfe644adb4631e
Oleg-Chalikov <o.chalikov@gmail.com>
2025-04-14 19:07:55 +0300

    Delete src directory

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const App = () => <h1 style={{ color: 'green', textAlign: 'center' }}>KISWEL MY-16R работает!</h1>
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
