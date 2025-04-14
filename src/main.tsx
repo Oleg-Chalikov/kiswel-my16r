72e002838a4acaedb5dfe3e74e1b452993343f15
Oleg-Chalikov <o.chalikov@gmail.com>
2025-04-14 19:01:06 +0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1 style={{ color: 'green', textAlign: 'center' }}>KISWEL MY-16R работает!</h1>
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
