d7f799b4c55265d94f9067ed8fc211c0e2e65141
Oleg-Chalikov <o.chalikov@gmail.com>
2025-04-16 21:45:20 +0300

    Delete src directory

--- src/LandingPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function LandingPage() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1 style={{ color: "lime" }}>Электрод KISWEL MY-16R</h1>
-      <p>Покрытый электрод для сварки корневого шва труб</p>
-    </div>
-  );
-}
